4a18db638a0ef9e8f11a75c85234cf52549f63bc
Jorjit Lee Sumintao <christy.paris@students.isatu.ph>
2023-09-20 04:21:40 +0000

    rwoorksheet

---
+++ RWorksheet_Sumintao#1.R
@@ -0,0 +1,21 @@
+age <-c(34,28,22,36,27,18,52,39,42,29,35,31,27,22,37,34,19,20,57,49,50,37,46,25,17,37,42,53,41,51,35,24,33,41)
+length (age)
+
+reciprocal_age <-1/age
+ reciprocal_age
+
+#new_age
+  new_age <-c(age,0,age)
+  new_age
+  
+#sort_the_age_values_for_age
+  sorted_age <-sort(age)
+  sorted_age
+  
+max (age)
+min (age)
+
+#6
+Age_vec <- c(2.4,2.8,2.1,2.5,2.4,2.2,2.5,2.3,2.5,2.3,2.4,2.7)
+
+lenght(Age_vec)
