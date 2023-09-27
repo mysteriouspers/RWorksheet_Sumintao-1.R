377315c75c788cafd00c7e68eae636e5639a43c6
Jorjit Lee Sumintao <christy.paris@students.isatu.ph>
2023-09-27 04:26:40 +0000

    first commit

---
+++ new.R
@@ -0,0 +1,127 @@
+#1
+#
+age <-c(34,28,22,36,27,18,52,39,42,29,35,31,27,22,37,34,19,20,57,49,50,37,46,25,17,37,42,53,41,51,35,24,33,41)
+length(age)
+
+B.
+reciprocal_age <-1/age
+reciprocal_age
+
+#3 assign new_age
+new_age <-c(age,0,age)
+new_age
+
+#4 sort age values
+sort_the_age_values_for_age
+sorted_age <-sort(age)
+sorted_age
+
+#5 
+max (age)
+
+min (age)
+
+#6
+
+Age_vec <- c(2.4,2.8,2.1,2.5,2.4,2.2,2.5,2.3,2.5,2.3,2.4,2.7)
+
+length (age_vec)
+
+
+#7
+age_vec <- c(2.4,2.8,2.1,2.5,2.4,2.2,2.5,2.3,2.5,2.3,2.4,2.7)
+
+length(age_vec)
+double<- age_vec
+
+#8.1
+seq_integer <-seq(1:100)
+length(seq_first)
+
+
+
+#8.2
+seq_second <-seq(20,60)
+c(20:60)
+
+
+#8.3 mean of numbers from from 20 to 60
+seq_third<-seq(20,60)
+
+
+#8.4 sum of numbers from 51 to 91
+
+sum(51:91)
+seq_first<-seq(51:91)
+
+#8.5 integers from 1 to 1000
+
+num_Vec <- seq(1,1000)
+num_Vec
+
+
+
+#c. for 8.5 find only maximum data points until 10
+
+max (data)
+
+#9 print a vector with integers between 1 and 100 that are not divisible by 3,5 and 7 using filter option
+
+Filter(function(i) { all(i %% c(3,5,7) != 0) }, seq(100))
+
+#10 generate a sequence backwards of the integers from 1 to 100. write the R code and its output
+
+100:1
+
+#11 list all the natural numbers below 25 that are multiples of 3 or 5. find the sum of these multiples.
+
+seq<- c(3, 6, 9, 12,15, 18, 21, 24)
+
+sum(3,6,9,12,15,18,21,24)
+
+
+#a. how many data points from 10 to 11
+
+data_point<-seq(10,11)
+length(data_point)
+
+
+
+
+
+
+
+
+
+#12
+
+{x <- 0+ x+ 5+}
+
+
+
+#13
+score <-c(72,86,92,63,88,91,75,75,77)
+score
+score_2 <- score_[2]
+score_3 <- score_3[3]
+score_2
+score_3
+
+
+
+
+#14 
+vector_1 <-c(1,2,NA,4,NA,6,7)
+print(vector_1,na.print="999")
+
+
+#15
+
+
+name=readline(prompt="input your name: ")
+
+age=readline(prompt="age: ")
+
+print(paste("My name is",name,"and i am",age,"years old."))
+
+print(R.version.string)
