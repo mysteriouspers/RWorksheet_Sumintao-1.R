6661b9b8a0aed46c236e2d43aa5c34ab9a13d9e0
Jorjit Lee Sumintao <christy.paris@students.isatu.ph>
2023-09-27 04:07:18 +0000

    RwOORKSHEET

--- RWorksheet_Sumintao#1.R
+++ RWorksheet_Sumintao#1.R
@@ -1,8 +1,7 @@
 #1
 
 age <-c(34,28,22,36,27,18,52,39,42,29,35,31,27,22,37,34,19,20,57,49,50,37,46,25,17,37,42,53,41,51,35,24,33,41)
-
-length (age)
+length(age)
 
 B.
 reciprocal_age <-1/age
@@ -18,29 +17,26 @@ reciprocal_age <-1/age
   sorted_age
  
 #5 
-  
 max (age)
 
 min (age)
 
 #6
-Age_vec <- c(2.4,2.8,2.1,2.5,2.4,2.2,2.5,2.3,2.5,2.3,2.4,2.7
-             
-lenght(Age_vec)
+
+Age_vec <- c(2.4,2.8,2.1,2.5,2.4,2.2,2.5,2.3,2.5,2.3,2.4,2.7)
+
+length (age_vec)
 
 
 #7
 age_vec <- c(2.4,2.8,2.1,2.5,2.4,2.2,2.5,2.3,2.5,2.3,2.4,2.7)
 
 length(age_vec)
-
-age_vec*2
+double<- age_vec
 
 #8.1
-
-seq_integer <- seq(1,100)
-
-lenght(seq_first)
+seq_integer <-seq(1:100)
+length(seq_first)
 
 
 
@@ -60,21 +56,9 @@ seq_first<-seq(51:91)
 
 #8.5 integers from 1 to 1000
 
-seq_integer <-seq(1,1000)
-lenght(seq_first)
+num_Vec <- seq(1,1000)
+num_Vec
 
-
-#a. how many data points from 8.1 t0 8.4?
-
-data_point<-c(8.1:8.4)
-
-length(8.1:8.4)
-
-
-#b write R code and its output from 8.1 to 8.4
-
-seq_integer <-seq(8.1:8.4)
-lenght(seq_first)
 
 
 #c. for 8.5 find only maximum data points until 10
@@ -91,36 +75,56 @@ Filter(function(i) { all(i %% c(3,5,7) != 0) }, seq(100))
 
 #11 list all the natural numbers below 25 that are multiples of 3 or 5. find the sum of these multiples.
 
+seq<- c(3, 6, 9, 12,15, 18, 21, 24)
+
+sum(3,6,9,12,15,18,21,24)
+
+
 #a. how many data points from 10 to 11
 
-seq_integer <-seq (10,11)
-lenght(seq_first)
+data_point<-seq(10,11)
+length(data_point)
 
 
-#12  
-{x <- 0+ x + 5 +}
 
 
-#13 
+
+
+
+
+
+#12
+
+{x <- 0+ x+ 5+}
+
+
+
+#13
+score <-c(72,86,92,63,88,91,75,75,77)
+score
+score_2 <- score_[2]
+score_3 <- score_3[3]
+score_2
+score_3
+
 
 
 
 #14 
-
-a_vec<- c(1,2,NA,4,NA,6,7)
-
-print(a,na,print="-999")
+vector_1 <-c(1,2,NA,4,NA,6,7)
+print(vector_1,na.print="999")
 
 
 #15
 
-name=readline(prompt="jorjit Lee Sumintao: ")
 
-age=readline(prompt="21: ")
+name=readline(prompt="input your name: ")
+
+age=readline(prompt="age: ")
 
 print(paste("My name is",name,"and i am",age,"years old."))
 
-printf(R.version.string)
+print(R.version.string)
 
 
 
